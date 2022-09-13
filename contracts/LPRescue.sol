c15482098bed56321707acc6219c919add8e8f85
Valentin Bersier <vbersier@gmail.com>
2022-09-13 18:43:13 +0200

    Added sortToken function that will be needed later

--- contracts/LPRescue.sol
+++ contracts/LPRescue.sol
@@ -9,8 +9,22 @@ contract LPRescue {
     IDexRouter public immutable router;
     IDexFactory public immutable factory;
 
+    /// Error while sorting the two token addresses from the pair
+    /// @param reason the reason code: 1 = identical addresses, 2 = zero address
+    error SortError(uint8 reason);
+
     constructor(address _router) {
         router = IDexRouter(_router);
         factory = IDexFactory(router.factory());
+    }
+
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        if (tokenA == tokenB) {
+            revert SortError(1); // identical addresses
+        }
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        if (token0 == address(0)) {
+            revert SortError(2); // zero address
+        }
     }
 }
