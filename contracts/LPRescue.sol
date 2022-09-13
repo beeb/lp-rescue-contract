2aec4c5628d24f7cbacd682491eb14dded6300da
Valentin Bersier <vbersier@gmail.com>
2022-09-13 18:27:25 +0200

    Added factory and router interfaces

--- contracts/LPRescue.sol
+++ contracts/LPRescue.sol
@@ -1,6 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+import "./interfaces/IDexRouter.sol";
+import "./interfaces/IDexFactory.sol";
+
 contract LPRescue {
     uint16 public constant VERSION = 1;
+    IDexRouter public immutable router;
+    IDexFactory public immutable factory;
+
+    constructor(address _router) {
+        router = IDexRouter(_router);
+        factory = IDexFactory(router.factory());
+    }
 }
