92632bc291d599425ba1bb65a0cceb0ba17930fe
Valentin Bersier <vbersier@gmail.com>
2022-09-14 08:06:03 +0200

    Bulk of the function is written now

--- contracts/LPRescue.sol
+++ contracts/LPRescue.sol
@@ -3,19 +3,108 @@ pragma solidity ^0.8.16;
 
 import "./interfaces/IDexRouter.sol";
 import "./interfaces/IDexFactory.sol";
+import "./interfaces/IDexPair.sol";
+import "./interfaces/IWETH.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract LPRescue {
+    using SafeERC20 for IERC20;
+
     uint16 public constant VERSION = 1;
     IDexRouter public immutable router;
     IDexFactory public immutable factory;
+    // solhint-disable-next-line var-name-mixedcase
+    address public immutable WETH;
 
-    /// Error while sorting the two token addresses from the pair
-    /// @param reason the reason code: 1 = identical addresses, 2 = zero address
+    /**
+    @notice Error while sorting the two token addresses from the pair
+    @param reason the reason code: 1 = identical addresses, 2 = zero address
+    */
     error SortError(uint8 reason);
+
+    /// @notice The pair doesn't exist yet
+    error PairNotCreated();
+
+    /// @notice The pair is not stuck, use the dex's addLiquidity function
+    error PairNotStuck();
+
+    /// @notice The message value is not sufficient to add the desired liquidity amount
+    error InsufficientValue();
+
+    event LPRescued(address tokenA, address tokenB, address pair);
 
     constructor(address _router) {
         router = IDexRouter(_router);
         factory = IDexFactory(router.factory());
+        WETH = router.WETH();
+    }
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountA,
+        uint256 amountB,
+        address to
+    )
+        external
+        payable
+        returns (
+            uint256 amountAActual,
+            uint256 amountBActual,
+            uint256 liquidity
+        )
+    {
+        /// @dev Perform a series of checks
+        address pairAddress = factory.getPair(tokenA, tokenB);
+        if (pairAddress == address(0)) {
+            // pair could still be stuck after creation if `sync` is called and there
+            // was one of the tokens at the contract address, but we don't handle creation here
+            revert PairNotCreated();
+        }
+        IDexPair pair = IDexPair(pairAddress);
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        if ((reserve0 == 0 && reserve1 == 0) || (reserve0 > 0 && reserve1 > 0)) {
+            // the pair is stuck when 1 reserve value is non-zero and the other is zero
+            revert PairNotStuck();
+        }
+        (address token0, address token1) = sortTokens(tokenA, tokenB); // check which is which
+        (uint256 amount0, uint256 amount1) = tokenA == token0 ? (amountA, amountB) : (amountB, amountA);
+
+        // one of the amounts transferred will be less than desired amount
+        // because one of the reserves is not zero
+        uint256 amount0Actual = amount0 - reserve0;
+        uint256 amount1Actual = amount1 - reserve1;
+        if ((token0 == WETH && msg.value < amount0Actual) || (token1 == WETH && msg.value < amount1Actual)) {
+            // check that the payable amount is enough
+            revert InsufficientValue();
+        }
+
+        /// @dev Transfer the tokens to the pair
+        // convert native ETH to WETH if needed
+        if (token0 == WETH) {
+            IWETH(token0).deposit{value: amount0Actual}();
+            IERC20(token0).safeTransfer(pairAddress, amount0Actual); // transfer WETH
+        } else {
+            IERC20(token0).safeTransferFrom(msg.sender, pairAddress, amount0Actual);
+        }
+
+        if (token1 == WETH) {
+            IWETH(token1).deposit{value: amount1Actual}();
+            IERC20(token1).safeTransfer(pairAddress, amount1Actual); // transfer WETH
+        } else {
+            IERC20(token1).safeTransferFrom(msg.sender, pairAddress, amount1Actual);
+        }
+
+        /// @dev We now mint the liquidity tokens
+        liquidity = pair.mint(to);
+
+        // return values
+        (amountAActual, amountBActual) = tokenA == token0
+            ? (amount0Actual, amount1Actual)
+            : (amount1Actual, amount0Actual);
+
+        emit LPRescued(tokenA, tokenB, pairAddress);
     }
 
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
