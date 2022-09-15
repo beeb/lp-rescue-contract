1e55bfc47e6899e3c21001b0aff7e5e34357b752
beeb <vbersier@gmail.com>
2022-09-15 20:26:36 +0200

    written deploy script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {},
     testnet: {
-      url: 'https://data-seed-prebsc-2-s3.binance.org:8545',
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
       chainId: 97,
       gasPrice: 20000000000,
       accounts: [pk_testnet],
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,7 +1,22 @@
 import { ethers } from 'hardhat'
+import hre from 'hardhat'
+import assert from 'assert'
+
+const router: Record<number, string> = {
+  97: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
+  56: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+}
 
 async function main() {
-  throw new Error('Not implemented')
+  console.log('Deploying on network: ', hre.network.name)
+
+  assert(hre.network.config.chainId && router[hre.network.config.chainId])
+
+  const Contract = await ethers.getContractFactory('LPRescue')
+  const contract = await Contract.deploy(router[hre.network.config.chainId])
+  await contract.deployed()
+
+  console.log('Contract deployed to:', contract.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
