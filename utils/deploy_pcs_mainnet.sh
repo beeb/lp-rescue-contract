d772044b5a165e21743244b2a355d06828d9a2d8
beeb <vbersier@gmail.com>
2023-06-14 23:11:25 +0200

    fix: mainnet rpc

--- utils/deploy_pcs_mainnet.sh
+++ utils/deploy_pcs_mainnet.sh
@@ -6,5 +6,5 @@ read -p "This script will deploy the contract to BSC mainnet, continue? [y/N] " 
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    forge script script/DeployPcsMainnet.s.sol:DeployPcs --rpc-url=bsctest --broadcast --verify -vvvv
+    forge script script/DeployPcsMainnet.s.sol:DeployPcs --rpc-url=bsc --broadcast --verify -vvvv
 fi
