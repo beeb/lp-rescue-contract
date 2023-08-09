5016ee935e87484f9abd3647e2ffab8b3f0e2382
beeb <703631+beeb@users.noreply.github.com>
2023-08-09 21:56:42 +0200

    chore(nix): use correct solc version

--- flake.nix
+++ flake.nix
@@ -3,14 +3,18 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     utils.url = "github:numtide/flake-utils";
     foundry.url = "github:shazow/foundry.nix/monthly"; # Use monthly branch for permanent releases
+    solc = {
+      url = "github:hellwolf/solc.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils, foundry }:
+  outputs = { self, nixpkgs, utils, foundry, solc }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ foundry.overlay ];
+          overlays = [ foundry.overlay solc.overlay ];
         };
       in
       {
@@ -18,11 +22,11 @@
         devShell = with pkgs; mkShell {
           buildInputs = [
             # From the foundry overlay
-            # Note: Can also be referenced without overlaying as: foundry.defaultPackage.${system}
             foundry-bin
 
             # ... any other dependencies we need
-            solc
+            solc_0_8_19
+            (solc.mkDefault pkgs solc_0_8_19)
           ];
         };
       });
